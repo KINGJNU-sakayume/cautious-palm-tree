5273d7edc8dedba10b2ced60e6321235981a80d1
KINGJNU-sakayume <213303@jnu.ac.kr>
2026-03-27 23:36:48 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cautious-palm-tree/',
+  base: '/cautious-palm-tree/', // 👈 이 줄이 반드시 있어야 합니다!
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
