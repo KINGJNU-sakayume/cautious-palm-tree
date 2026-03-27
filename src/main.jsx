4032c11bd5e5ec186183ff5daa6556b1ae2785bc
KINGJNU-sakayume <213303@jnu.ac.kr>
2026-03-27 23:37:10 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // 👈 BrowserRouter에서 변경
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> {/* 👈 여기도 HashRouter로 변경 */}
+    <HashRouter> {/* 👈 여기도 변경 */}
       <AppProvider>
         <ToastProvider>
           <App />
